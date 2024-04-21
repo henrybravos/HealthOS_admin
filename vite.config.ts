88007bf8fa370b1bc6dacf286d6a1c39969228ee
Henry Bravo <hbravos.info@gmail.com>
2024-04-20 23:06:18 -0500

    :recycle: sort, install mui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { URL, fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@features": fileURLToPath(new URL("./src/features", import.meta.url)),
+      "@common": fileURLToPath(new URL("./src/common", import.meta.url)),
+      "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@routes": fileURLToPath(new URL("./src/routes", import.meta.url)),
+      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+      "@context": fileURLToPath(new URL("./src/context", import.meta.url))
+    }
+  },
+  server: {
+    host: true
+  }
 })
